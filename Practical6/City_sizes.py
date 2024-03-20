1505f2d76d434f45fabccbf31cc58333f5e2efc9
zhewen-wenxuan <18858588688@163.com>
2024-03-20 13:47:15 +0800

    City_sizes.py

--- Practical6/City_sizes.py
+++ Practical6/City_sizes.py
@@ -1,23 +1,19 @@
 uk_cities=[0.56,0.62,0.04,9.7]
 ch_cities=[0.58,8.4,29.9,22.2]
-import numpy as np
+uk_cityname=["Edinburgh","Glasgow","Stirling","London"]
+china_cityname=["Haining","Hangzhou","Shanghai","Beijing"]
 import matplotlib.pyplot as plt
-N=4
-std_err = [2, 1, 2, 3]
-ind = np.arange(N)
 width = 0.5
 plt.figure()
-plt.bar(ind,uk_cities, width, yerr=std_err)
+plt.bar(uk_cityname,uk_cities, width)
 plt.ylabel("Population")
 plt.title("Population of UK cities")
-plt.xticks(ind)
 plt.show()
 
 
 plt.figure()
-plt.bar(ind,ch_cities, width, yerr=std_err)
+plt.bar(china_cityname,ch_cities, width)
 plt.ylabel("Population")
 plt.title("Population of China cities")
-plt.xticks(ind)
 plt.show()
 plt.clf()

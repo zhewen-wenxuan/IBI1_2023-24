50cf439b73e9a68259e1f205b03d77cd605a1ea1
zhewen-wenxuan <18858588688@163.com>
2024-04-09 22:12:03 +0800

    repetitive_counter.py

--- Practical8/repetitive_counter.py
+++ Practical8/repetitive_counter.py
@@ -1,1 +1,12 @@
+import re
+def function_count(seq):
+  List1=re.findall('GTGTGT',seq)
+  List2=re.findall('GTCTGT',seq)
+  count1=len(List1)
+  count2=len(List2)
+  total_count=count1+count2
+  return total_count
+
 seq='ATGCAATCGGTGTGTCTGTTCTGAGAGGGCCTAA'
+answer=function_count(seq)
+print(answer)

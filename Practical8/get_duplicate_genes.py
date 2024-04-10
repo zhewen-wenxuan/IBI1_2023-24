c63bfa0a0248d6abb0efd983c0527779b07e29e1
zhewen-wenxuan <18858588688@163.com>
2024-04-10 15:06:55 +0800

    duplicate_genes.fa & get_duplicate_genes.py

--- Practical8/get_duplicate_genes.py
+++ Practical8/get_duplicate_genes.py
@@ -1,2 +1,13 @@
 import re
-with open(Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa,'r')
+input_file='Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa'
+output_file='duplicate_genes.fa'
+pattern = re.compile(r'^>(\w+)_.*protein_coding.*$')
+with open(input_file,'r') as fin, open(output_file,'w') as fout:
+    for line in fin:
+        match = pattern.match(line)
+        if match:
+            gene_name = match.group()  
+            simplified_line = f">{gene_name}\n"  
+            fout.write(simplified_line) 
+        else:
+            fout.write(line)

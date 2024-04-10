8d1273f9944f316d2b2e3687a353039f433c7127
zhewen-wenxuan <18858588688@163.com>
2024-04-10 15:41:55 +0800

    bbb

--- Practical8/get_duplicate_genes.py
+++ Practical8/get_duplicate_genes.py
@@ -1,12 +1,14 @@
 import re
 input_file='/Users/xuanzhewen/code/IBI1_2023-24/Practical8/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa'
 output_file='/Users/xuanzhewen/code/IBI1_2023-24/Practical8/duplicate_genes.fa'
-pattern = re.compile(r'^>(\w+)_.*protein_coding.*$')
+pattern1 = re.compile(r'^>(\w+)_.*duplication.*$')
+pattern2 = re.compile(r'^>(\w+)_.*(?!duplication).*$')
 with open(input_file,'r') as fin, open(output_file,'w') as fout:
     for line in fin:
-        match = pattern.match(line)
+        match = pattern1.match(line)
+        unmatch = pattern2.match(line)
         if match:
-            gene_name = match.group()  
+            gene_name = match.group(1)  
             simplified_line = f">{gene_name}\n"  
             fout.write(simplified_line) 
         else:

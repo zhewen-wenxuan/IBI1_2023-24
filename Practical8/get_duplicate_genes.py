d513bc97f260c60d1e6b981311543f1f25dcf91d
zhewen-wenxuan <18858588688@163.com>
2024-04-10 21:06:25 +0800

    dulicate_genes.fa & get_duplicate_genes.py

--- Practical8/get_duplicate_genes.py
+++ Practical8/get_duplicate_genes.py
@@ -1,15 +1,20 @@
 import re
 input_file='/Users/xuanzhewen/code/IBI1_2023-24/Practical8/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa'
 output_file='/Users/xuanzhewen/code/IBI1_2023-24/Practical8/duplicate_genes.fa'
-pattern1 = re.compile(r'^>(\w+)_.*duplication.*$')
-pattern2 = re.compile(r'^>(\w+)_.*(?!duplication).*$')
+pattern = re.compile(r'^>(\w+)_.*$')
+flag=False #To choose the sequence only under duplication genes
 with open(input_file,'r') as fin, open(output_file,'w') as fout:
     for line in fin:
-        match = pattern1.match(line)
-        unmatch = pattern2.match(line)
+        match = pattern.match(line)
         if match:
-            gene_name = match.group(1)  
-            simplified_line = f">{gene_name}\n"  
-            fout.write(simplified_line) 
+            if "duplication" in line:
+                gene_name = match.group(1)  
+                simplified_line = f">{gene_name}\n"  
+                fout.write(simplified_line) 
+                flag=True
+            else:
+                flag=False
         else:
-            fout.write(line)
+            if flag:
+                fout.write(line)
+            

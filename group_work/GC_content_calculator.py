f13458c2aef6e648c77055378fd5f9b7d83ba62d
zhewen-wenxuan <18858588688@163.com>
2024-04-30 12:53:49 +0800

    GC_content

--- group_work/GC_content_calculator.py
+++ group_work/GC_content_calculator.py
@@ -3,9 +3,7 @@ def DNA(dna):
     b=len(dna)
     c=0
     for i in range (b):
-        if a[i]=="C":
-            c=c+1
-        if a[i]=="G":
+        if a[i]=="C" or a[i]=="G":
             c=c+1
     return print(c*100/b,"%")
 x=input('DNA sequence:')

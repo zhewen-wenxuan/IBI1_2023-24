af15bae3acaaf29513a9d899ffa192ef1b4fb4f5
zhewen-wenxuan <18858588688@163.com>
2024-05-15 14:47:25 +0800

    1

---
+++ group_work/DNA_to_mRNA_covertor1.py
@@ -0,0 +1,15 @@
+def DNA_mRNA_convertor(dna):
+    a=list(dna)
+    b=[]
+    for i in range(len(dna)):
+        if a[i]=="A":
+            b.append("U")
+        if a[i]=="T":
+            b.append("A")
+        if a[i]=="C":
+            b.append("G")
+        if a[i]=="G":
+            b.append("C")
+    return print("".join(b))
+x=''
+DNA_mRNA_convertor (x)

914aee7d8ab1f34f750dc03a66893e2a22a96ddb
zhewen-wenxuan <18858588688@163.com>
2024-05-19 13:46:55 +0800

    Practical6

--- Practical6/Average_day.py
+++ Practical6/Average_day.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
-e=input()
-Dic={'Sleeping':8,'Classes':6,'Studying':3.5,'TV':2,'Music':1,'strength training100':e} #Compile data into a dictionary
+Dic={'Sleeping':8,'Classes':6,'Studying':3.5,'TV':2,'Music':1,'other':24-8-6-3.5-2-1} #Compile data into a dictionary
 Sp= [0, 0, 0.1, 0, 0, 0,] #emphasis one activity
 a=Dic.keys() #a is the keys of Dic
 b=Dic.values() #b is values of Dic
 plt.figure()
-plt.pie(b, labels =a, startangle = 90, explode =Sp)
-plt.show()
+plt.pie(b, labels =a, startangle = 90, explode =Sp)  # create the par chart, and the "study" part will be moved out a little bit
+plt.show() # show the image
 plt.clf() #Clear Previous Image
+activity=str(input("Input the activity you want to know (From 'sleep','classes','study','TV','music','other'):"))
+print("You spent",Dic[activity],"hours on",activity,"during an average day.") # output the resule
+
--- Practical6/City_sizes.py
+++ Practical6/City_sizes.py
@@ -2,15 +2,17 @@ uk_cities=[0.56,0.62,0.04,9.7] #list the population and city name
 ch_cities=[0.58,8.4,29.9,22.2]
 uk_cityname=["Edinburgh","Glasgow","Stirling","London"]
 china_cityname=["Haining","Hangzhou","Shanghai","Beijing"]
+uk_cities.sort()  # sort values from smaller to bigger for cities in the UK
+ch_cities.sort(reverse=True)  # sort values from bigger to smaller for cities in China
 import matplotlib.pyplot as plt 
 width = 0.5 #leave proper space
 print(uk_cities,ch_cities)
+
 plt.figure()
 plt.bar(uk_cityname,uk_cities, width)
 plt.ylabel("Population")
 plt.title("Population of UK cities") #set a proper title
 plt.show()
-
 
 plt.figure()
 plt.bar(china_cityname,ch_cities, width)

473eaae44f065f366b656f3197e59f76881647c9
zhewen-wenxuan <18858588688@163.com>
2024-05-20 19:00:37 +0800

    spatial_SIR.py

--- Practical10/spatial_SIR.py
+++ Practical10/spatial_SIR.py
@@ -1,39 +1,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
-# make array of all susceptible population
-N=10000
-Sus=9999
-Inf=1
-Rec=0
-beta=0.2
-gamma=0.05
-population =np.zeros((100,100))
-outbreak=np.random.choice(range(100),2)
-population[outbreak[0],outbreak[1]]=1
-for i in range(100):
-    # find infected points
-    infectedIndex = np.where(population==1)
-    # loop through all infected points
-    for i in range(len(infectedIndex[0])):
-        # get x, y coordinates for each point
-        x = infectedIndex[0][i]
-        y = infectedIndex[1][i]
-        # infect each neighbour with probability beta
-        # infect all 8 neighbours (this is a bit finicky, is there a better way?):
-        for xNeighbour in range(x-1,x+2):
-            for yNeighbour in range(y-1,y+2):
-                # don't infect yourself! (Is this strictly necessary?)
-                if (xNeighbour,yNeighbour) != (x,y):
-                    # make sure I don't fall off an edge
-                    if xNeighbour != -1 and yNeighbour != -1 and xNeighbour!=100 and yNeighbour!=100:
-                        # only infect neighbours that are not already infected!
-                        if  population[xNeighbour,yNeighbour]==0:
-                            population[xNeighbour,yNeighbour]=np.random.choice(range(2),1,p=[1-beta,beta])[0]
+
+N = 10000  # Total population size
+beta = 0.3  # Infection rate
+gamma = 0.05  # Recovery rate
+
+# Initialize population matrix: 0=Susceptible, 1=Infected, 2=Recovered
+population = np.zeros((100, 100))
+
+# Set initial outbreak
+outbreak = np.random.choice(range(100), 2)
+population[outbreak[0], outbreak[1]] = 1  # Mark the outbreak point as infected
+
+# Simulate the spread
+for t in range(101):
+    new_population = population.copy()  # Copy the current state
+    
+    # Find all infected points
+    infectedIndex = np.where(population == 1)
+    
+    # Infect neighbors
     for i in range(len(infectedIndex[0])):
         x = infectedIndex[0][i]
         y = infectedIndex[1][i]
-        population[x,y]=np.random.choice(range(3),1,p=[0,1-gamma,gamma])[0]
-    if i % 25==0:
-        plt.figure(figsize=(6,4),dpi=150)
-        plt.imshow(population,cmap='viridis',interpolation='nearest')
+        
+        # Infect all 8 neighbors(this is a bit finicky, is there a better way?)
+        for xNeighbour in range(x-1, x+2):
+            for yNeighbour in range(y-1, y+2):
+                # Ensure not infecting itself and within bounds(Is this strictly necessary?)
+                if (xNeighbour, yNeighbour) != (x, y) and 0 <= xNeighbour < 100 and 0 <= yNeighbour < 100:
+                    # Only infect susceptible neighbors
+                    if population[xNeighbour, yNeighbour] == 0:
+                        if np.random.rand() < beta:
+                            new_population[xNeighbour, yNeighbour] = 1  # Infect susceptible neighbor
+    
+    # Recover infected individuals
+    for i in range(len(infectedIndex[0])):
+        x = infectedIndex[0][i]
+        y = infectedIndex[1][i]
+        if np.random.rand() < gamma:
+            new_population[x, y] = 2  # Mark the infected individual as recovered
+    
+    # Update the population matrix
+    population = new_population.copy()
+    
+    # Plot the population state at specific time steps
+    if t in [0, 10, 50, 100]:
+        plt.figure(figsize=(6, 4), dpi=150)
+        plt.imshow(population, cmap='viridis', interpolation='nearest')
+        plt.title(f'Time step: {t}')
+        plt.colorbar(label='State (0=Susceptible, 1=Infected, 2=Recovered)')
         plt.show()

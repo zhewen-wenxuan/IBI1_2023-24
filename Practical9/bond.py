b307910e7da905e4e4c05be804453cd8a3281363
zhewen-wenxuan <18858588688@163.com>
2024-05-20 12:45:47 +0800

    bond.py

--- Practical9/bond.py
+++ Practical9/bond.py
@@ -1,13 +1,15 @@
-def favorite_bond(birth_year):
-    actors = {"Roger Moore": 1973,"Timothy Dalton": 1987,"Pierce Brosnan": 1995,"Daniel Craig": 2006}
-    for actor,year in actors.items():
-        if birth_year >=year:
-            return actor
-        else:
-            return "None"
-        
-b=1988
-print(favorite_bond(b))
+def name_Bond(x):
+    y=x+18               # Calculate the age when the person turned 18.
+    if 1973<=y<=1986:
+        print("The actor is Roger Moore.")
+    if 1987<=y<=1994:
+        print('The actor is Timothy Dalton.')
+    if 1995<=y<=2005:
+        print('The actor is Pierce Brosnan.')
+    if 2006<=y<=2021:
+        print('The actor is Daniel Craig.')
+    if (y<1973) or (y>2021):
+        print('No actor according to the practical')
+x=int(input("The year you born:"))
+name_Bond(x)
 
-a=int(input('Your birth'))
-print(favorite_bond(a)) 

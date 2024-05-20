7d67b79fa59e5c72c689af13446827623c29f4ad
zhewen-wenxuan <18858588688@163.com>
2024-05-20 19:14:47 +0800

    Alignment.py

--- Practical13/Alignment.py
+++ Practical13/Alignment.py
@@ -7,13 +7,13 @@ human_str=''
 mouse_str=''
 rat_str=''
 
-human_str = ""  # 在循环外初始化 human_str
+human_str = ""#Initialize human_str outside of the loop
 for line in human:
-    line = line.strip()  # 去除行首和行尾的空格，包括 '\n'
+    line = line.strip()  # Remove spaces at the beginning and end of a line, including '\ n'
     if line.startswith('>'):
-        human_str = ""  # 如果是标题行，重置 human_str
+        human_str = ""  # If it is a title line, reset human_str
     else:
-        human_str += line  # 将行连接到 human_str
+        human_str += line  # Connect rows to human_str
 mouse_str=""
 for line in mouse:
     line = line.strip()
@@ -45,16 +45,26 @@ for i in range(len(rat_str)):
     score_1+=matrix[human_str[i]][mouse_str[i]]
     if human_str[i]==mouse_str[i]:
         amino_1+=1
+identical_percentage1=amino_1/len(rat_str)
+print('BLOSUM score for human mouse comparison is ', score_1)
+print('identity  for human mouse comparison is', identical_percentage1)
 for i in range(len(rat_str)):
     score_2+=matrix[human_str[i]][rat_str[i]]
     if human_str[i]==rat_str[i]:
         amino_2+=1
+identical_percentage2=amino_2/len(rat_str)
+print('BLOSUM score for human rat comparison is ', score_2)
+print('identity  for human rat comparison is', identical_percentage2)
 for i in range(len(rat_str)):
     score_3+=matrix[rat_str[i]][mouse_str[i]]
     if rat_str[i]==mouse_str[i]:
         amino_3+=1
-print(score_1,amino_1,score_2,amino_2,score_3,amino_3)
+identical_percentage3=amino_3/len(rat_str)
+print('BLOSUM score for rat mouse comparison is ', score_3)
+print('identity  for rat mouse comparison is', identical_percentage3)
 
+print('mouse sequence and rat sequence are most relative')
+print('mouse is better than rat to be a organism model')
 # mouse sequence and rat sequence are most relative
 # mouse is better than rat to be a organism model   
 
